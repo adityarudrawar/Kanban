9d516234324e2864e720b6031db86142a6682c1d
Aditya <adirudrawar17@gmail.com>
2021-09-02 12:54:43 -0400

    Push to Github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBJJZg1tsCemmWS7A0AjgtoI_smnX8SR3E",
+    authDomain: "my-dev-app-afa51.firebaseapp.com",
+    projectId: "my-dev-app-afa51",
+    storageBucket: "my-dev-app-afa51.appspot.com",
+    messagingSenderId: "534685715828",
+    appId: "1:534685715828:web:46ba6dd384c19dd368f021",
+    measurementId: "G-LJQQWZL57N"  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {  
+  apiKey: "AIzaSyBJJZg1tsCemmWS7A0AjgtoI_smnX8SR3E",
+  authDomain: "my-dev-app-afa51.firebaseapp.com",
+  projectId: "my-dev-app-afa51",
+  storageBucket: "my-dev-app-afa51.appspot.com",
+  messagingSenderId: "534685715828",
+  appId: "1:534685715828:web:46ba6dd384c19dd368f021",
+  measurementId: "G-LJQQWZL57N"
+}
 };
 
 /*
